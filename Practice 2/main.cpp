3651114be7a5aaa94487a44a3a567b9055c07090
Tysha <115850208+aoideee@users.noreply.github.com>
2023-10-08 14:42:06 -0600

    Create main.cpp

---
+++ Practice 2/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+void display();//function declaration
+
+int main()
+{
+    display();
+}
+
+void display()//function implementation
+{
+    cout<<"Tysha";
+}
