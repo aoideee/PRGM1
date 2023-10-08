7315a69ee714a53380deafce9f70f7f9c9ce6c0e
Tysha <115850208+aoideee@users.noreply.github.com>
2023-10-08 12:44:34 -0600

    Create main.cpp

---
+++ FoC Challenge/find the sum of two numbers/main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n1;
+    int n2;
+    int add;
+    
+    //Input:
+    cout << "Input the first number: ";
+    cin >> n1;
+    
+    cout << "Input the second number: ";
+    cin >> n2;
+    
+    //Processing:
+    add = n1 + n2;
+    
+    //Output:
+    cout << "The sum of " << n1 << " and " << n2 << " is " << add;
+    
+
+    return 0;
+}
