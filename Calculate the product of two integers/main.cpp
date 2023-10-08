ed2b3f76ef74e6a0badeeb89724988497dda9917
Tysha <115850208+aoideee@users.noreply.github.com>
2023-10-08 14:38:25 -0600

    Create main.cpp

---
+++ Calculate the product of two integers/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int x;
+    int y;
+    int result;
+    
+    cout<<"Enter number 1: ";
+    cin>>x;
+    
+    cout<<"Enter number 2: ";
+    cin>>y;
+    
+    result=x*y;
+    
+    cout<<"The product is "<<result<<".";
+
+    return 0;
+}
