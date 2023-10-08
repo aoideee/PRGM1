73aa8ab5ba7aed53d6c0bd529bfad069d33b9333
Tysha <115850208+aoideee@users.noreply.github.com>
2023-10-08 14:23:17 -0600

    Create main.cpp

---
+++ FoC Challenge/Find the Sum of First Five Natural Numbers/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    int f;
+    
+    //Input
+    n=5;
+    
+    //Processing
+    f = (n*(n+1))/2;
+    
+    //Output
+    cout<<"The sum of the first 5 natural numbers is "<<f<<".";
+
+    return 0;
+}
