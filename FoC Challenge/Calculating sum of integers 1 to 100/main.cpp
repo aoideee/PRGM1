26ad54d6b1d05e674e078434fa68015e4e064380
Tysha <115850208+aoideee@users.noreply.github.com>
2023-10-08 14:25:24 -0600

    Create main.cpp

---
+++ FoC Challenge/Calculating sum of integers 1 to 100/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    int f;
+    
+    //Input
+    n=100;
+    
+    //Processing
+    f = (n*(n+1))/2;
+    
+    //Output
+    cout<<"The sum of integers 1 to 100 is "<<f<<".";
+
+    return 0;
+}
