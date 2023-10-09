91833fd9c29692f4ab7b805cf53bbac79a79f5f1
Tysha <115850208+aoideee@users.noreply.github.com>
2023-10-08 19:06:18 -0600

    Create main.cpp

---
+++ Make into a function/main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+
+
+int main()
+{
+    int i=0;
+    int base=6;
+    int expo=5;
+    int answer=1;
+    
+    
+while(i<expo)
+{
+   answer=answer*base;
+   cout<<answer<<"\n";
+   i=i+1;
+        
+    }    
+    
+    
+
+    return 0;
+}
